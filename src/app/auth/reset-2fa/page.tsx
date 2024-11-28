cd62fb937ca844258c1d1b0209f5e7bbabf4d10a
dik-ab <abedaiki06@gmail.com>
2024-11-28 17:32:09 +0900

    🔨 2次元パスワードリセット画面のpath追加

---
+++ src/app/auth/reset-2fa/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import MainContent from '@/components/reset-2fa/MainContent'
+import React from 'react'
+
+const Reset2FA = () => {
+  return (
+    <MainContent />
+  )
+}
+
+export default Reset2FA
